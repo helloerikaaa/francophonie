4e4c98d9348f0947503739a132b85c51772cc0db
Erika Sánchez Femat <erikasafe@gmail.com>
2020-04-29 15:45:33 +0200

    Main scrren

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import anime from 'animejs/lib/anime.es.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'francophonie';
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    anime({
+      targets: '#world-img',
+      rotate: '360',
+      duration: '3000',
+      loop:true
+    });
+  }
 }
